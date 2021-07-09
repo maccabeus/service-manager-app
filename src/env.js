17fdf763fc1a84fdb4d212d88e9216116dd5351b
maccaabeus <mlisoftinc@gmail.coms>
2021-07-09 03:29:24 +0100

    deply to pythonanywhere

--- src/env.js
+++ src/env.js
@@ -1,3 +1,3 @@
 //export const BASE_API_URL="http://localhost:8000"
 // export const BASE_API_URL="https://schedule-service-manager.herokuapp.com"
-export const BASE_API_URL="http://maccabeus.pythonanywhere.com"
+export const BASE_API_URL="https://maccabeus.pythonanywhere.com"
